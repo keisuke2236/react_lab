de47979fb8422a2920557e7065ff1905a5a14a32
rorensu2236 <rorensu2236@gmail.com>
2024-02-07 19:04:35 +0900

    各コンテンツごとにURLで出しわけ

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React, { Suspense, useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  let query = new URLSearchParams(window.location.search);
+  let chapter = query.get('chapter') || 'default';
+
+  // 例：http://localhost:5174/?chapter=1
+  const ChapterComponent = React.lazy(
+    () => import(`./Chapter${chapter}/App`).catch(() => (
+      { default: () => <div>Chapter not found.</div> })
+    )
+  );
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div>
+      <Suspense fallback={<div>Loading chapter...</div>}>
+        <ChapterComponent />
+      </Suspense>
+    </div>
+  );
 }
 
-export default App
+export default App;
