0f04720d9e1dfc06d91887f93275ba21022ab9f6
rorensu2236 <rorensu2236@gmail.com>
2024-02-08 13:07:42 +0900

    チャプター5

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,24 @@ function App() {
   const chapter = useContext(ChapterContext);
 
   // 例：http://localhost:5174/?chapter=1
-  const ChapterComponent = React.lazy(
-    () => import(`./Chapter${chapter}/App`).catch(() => (
-      { default: () => <div>Chapter not found.</div> })
-    )
+  const ChapterComponent = React.lazy(() =>
+    import(`./Chapter${chapter}/App`)
+      .then(module => ({ default: module.default }))
+      .catch(error => {
+        console.error("インポートエラー:", error);
+        return {
+          default: () =>
+            <div>チャプターの読み込みに失敗しました。
+              <br />{error.message}
+            </div>
+        };
+      })
   );
 
   return (
     <div>
       <ChapterContext.Provider value={chapter}>
-        <Suspense fallback={<div>Loading chapter{chapter}...</div>}>
+        <Suspense fallback={<div>読み込み中{chapter}...</div>}>
           <ChapterComponent />
         </Suspense>
       </ChapterContext.Provider>
