632d6dd66c027ff46af462ec11ed6ba870191dc4
rorensu2236 <keisuke.terada@livesense.co.jp>
2024-09-06 22:15:40 +0900

    biomeに切り替え

--- src/chapters/_chapterXX/ChapterXX.tsx
+++ src/chapters/_chapterXX/ChapterXX.tsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import { ChapterContext } from '../../lib/ChapterContext';
+import { useContext } from "react";
+import { ChapterContext } from "../../lib/ChapterContext";
 
 export default function App() {
-  const chapter = useContext(ChapterContext);
-  return (
-    <>
-      <h1>Chapter {chapter}：チャプタータイトル</h1>
-      <div className="flex flex-wrap justify-center items-center gap-4 p-4">
-        <div className="flex-grow basis-1/2 md:basis-1/3 lg:basis-1/4 p-4 m-2">
-          {/* コンテンツ */}
-        </div>
-      </div>
-    </>
-  );
+	const chapter = useContext(ChapterContext);
+	return (
+		<>
+			<h1>Chapter {chapter}：チャプタータイトル</h1>
+			<div className="flex flex-wrap justify-center items-center gap-4 p-4">
+				<div className="flex-grow basis-1/2 md:basis-1/3 lg:basis-1/4 p-4 m-2">
+					{/* コンテンツ */}
+				</div>
+			</div>
+		</>
+	);
 }
--- src/lib/ConsoleLogViewer.tsx
+++ src/lib/ConsoleLogViewer.tsx
@@ -1,37 +1,51 @@
 import { useEffect, useState } from "react";
 
 export const ConsoleLogViewer: React.FC = () => {
-  const [logs, setLogs] = useState<string[]>([]);
-  const [width, setWidth] = useState<string>("600px");
-  const widthList = ["1000px", "800px", "600px", "400px", "200px"];
+	const [logs, setLogs] = useState<string[]>([]);
+	const [width, setWidth] = useState<string>("600px");
+	const widthList = ["1000px", "800px", "600px", "400px", "200px"];
 
-  const WidthChangeButton: React.FC<{ width: string }> = ({ width }) => (
-    <button onClick={() => setWidth(width)} className="mb-2 p-1 pt-0 pb-0 mr-1">{width}</button>
-  );
+	const WidthChangeButton: React.FC<{ width: string }> = ({ width }) => (
+		<button onClick={() => setWidth(width)} className="mb-2 p-1 pt-0 pb-0 mr-1">
+			{width}
+		</button>
+	);
 
-  useEffect(() => {
-    const originalConsoleLog = console.log;
-    console.log = (...args: any[]) => {
-      setLogs(prevLogs => [...prevLogs, args.map(arg => 
-        typeof arg === 'object' ? JSON.stringify(arg) : String(arg)
-      ).join(' ')]);
-      originalConsoleLog.apply(console, args);
-    };
+	useEffect(() => {
+		const originalConsoleLog = console.log;
+		console.log = (...args: any[]) => {
+			setLogs((prevLogs) => [
+				...prevLogs,
+				args
+					.map((arg) =>
+						typeof arg === "object" ? JSON.stringify(arg) : String(arg),
+					)
+					.join(" "),
+			]);
+			originalConsoleLog.apply(console, args);
+		};
 
-    return () => {
-      console.log = originalConsoleLog;
-    };
-  }, []);
+		return () => {
+			console.log = originalConsoleLog;
+		};
+	}, []);
 
-  return (
-    <div className="fixed top-0 right-0 h-full bg-gray-500 p-2 overflow-auto" style={{ width }}>
-      <h2 className="text-lg font-bold mb-2">Console Logs</h2>
-      {widthList.map(width => ( <WidthChangeButton key={width} width={width} /> ))}
-      <div className="rounded-md bg-black mb-2 p-2">
-        {logs.map((log, index) => (
-          <div key={index} className="mb-1 font-mono text-sm">{log}</div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="fixed top-0 right-0 h-full bg-gray-500 p-2 overflow-auto"
+			style={{ width }}
+		>
+			<h2 className="text-lg font-bold mb-2">Console Logs</h2>
+			{widthList.map((width) => (
+				<WidthChangeButton key={width} width={width} />
+			))}
+			<div className="rounded-md bg-black mb-2 p-2">
+				{logs.map((log, index) => (
+					<div key={index} className="mb-1 font-mono text-sm">
+						{log}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
